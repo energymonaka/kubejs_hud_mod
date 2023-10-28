06b0f2512e5036a9a8b015567c5a079601ec5b64
zprl <zprl.dev@gmail.com>
2023-10-28 13:49:58 +0900

    update

--- client_scripts/script.js
+++ client_scripts/script.js
@@ -1,20 +1,27 @@
-onEvent("client.tick", (event) => {
+onEvent("player.logged_in", (event) => {
   event.player.paint({
-    coordinates: {
+    client_xyz: {
       type: "text",
-      text: `
-			${String(Math.round(event.player.x))} 
-			${String(Math.round(event.player.y))} 
-			${String(Math.round(event.player.z))}
-			`,
       scale: 1.2,
       x: 0,
-      y: event.player.creativeMode ? -22 : -38,
       alignX: "center",
       alignY: "bottom",
       draw: "ingame",
       color: "#BCBCBC",
       shadow: true,
+    },
+  });
+});
+onEvent("client.tick", (event) => {
+  const xyz_text = `
+  ${Math.round(event.player.x)}
+  ${Math.round(event.player.y)}
+  ${Math.round(event.player.z)}
+   `;
+  event.player.paint({
+    client_xyz: {
+      text: xyz_text,
+      y: event.player.creativeMode ? -22 : -38,
     },
   });
 });
